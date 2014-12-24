b4f637ba3228888229afa93a2bd6a7db172ad34f
Nick Teleky <nteleky@gmail.com>
2014-12-24 18:20:43 -0500

    Added render function for stats

--- js/views/todolist.js
+++ js/views/todolist.js
@@ -5,6 +5,13 @@ app.TodoListView = Backbone.View.extend({
 
     statsTemplate: _.template( $('#stats-template').html()),
 
+    //Delegated events for creating new items, clearing completed ones, etc.
+    events: {
+        'keypress #new-todo': 'createOnEnter',
+        'click #clear-completed': 'clearCompleted',
+        'click #toggle-all': 'toggleAllComplete'
+    },
+
     //Bind relevant events on todos collection
     initialize: function() {
         this.allCheckbox = this.$('#toggle-all')[0];
@@ -14,6 +21,37 @@ app.TodoListView = Backbone.View.extend({
 
         this.listenTo(app.Todos, 'add', this.addOne);
         this.listenTo(app.Todos, 'reset', this.addAll);
+
+        this.listenTo(app.Todos, 'change:completed', this.filterOne);
+        this.listenTo(app.Todos, 'filter', this.filterAll);
+        this.listenTo(app.Todos, 'all', this.render);
+
+        app.Todos.fetch();
+    },
+
+    render: function() {
+        var completed = app.Todos.completed().length;
+        var remaining = app.Todos.remaining().length;
+
+        if (app.Todos.length) {
+            this.$main.show();
+            this.$footer.show();
+
+            this.$footer.html(this.statsTemplate({
+                completed: completed,
+                remaining: remaining
+            }));
+
+            this.$('#filters li a')
+                .removeClass('selected')
+                .filter('[href="#/' + (app.TodoFilter || '') + '"]')
+                .addClass('selected');
+        } else {
+            this.$main.hide();
+            this.$footer.hide();
+        }
+
+        this.allCheckbox.checked = !remaining;
     },
 
     //Add a single todo item to the list by creating a view
